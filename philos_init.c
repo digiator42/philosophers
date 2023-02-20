14284813ee9f40e1d29b2b8a033a251372da2c1d
Ahmed Hassan <digiator2020@gmail.com>
2023-02-20 12:23:18 +0400

    ids

--- philos_init.c
+++ philos_init.c
@@ -6,7 +6,7 @@
 /*   By: ahassan <ahassan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 22:43:25 by ahassan           #+#    #+#             */
-/*   Updated: 2023/02/20 12:18:26 by ahassan          ###   ########.fr       */
+/*   Updated: 2023/02/20 12:23:17 by ahassan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void *routine(void *arg)
 {
 	printf("hi from routine %d\n", *(int*)arg);
+	// if()
 	return NULL;
 }
 
@@ -29,7 +30,7 @@ void philo_init(t_philos *philos, t_philo *input)
 	philos->philo = malloc(sizeof(t_philo) * input->num_of_philos);
 	while(*in.i < input->num_of_philos)
 	{
-		if(*in.i % 2 == 0)
+		if(!(*in.i % 2))
 		{
 			philos->philo[*in.i].id = *in.i + 1;
 			pthread_create(&philos->philo[*in.i].thread, NULL, &routine, in.i);
